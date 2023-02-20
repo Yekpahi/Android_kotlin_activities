78259f449117fa3a4a8da8cc2c0fd3ecd1924309
Yekpahi Prince <princeyekpahi@hotmail.fr>
2023-02-20 03:47:15 +0100

    nest CyclerView

---
+++ NestRecycle/app/src/main/java/com/example/nestrecycle/ChildItem.kt
@@ -0,0 +1,3 @@
+package com.example.nestrecycle
+
+data class ChildItem(val title : String, val logo : Int)
---
+++ NestRecycle/app/src/main/java/com/example/nestrecycle/ParentItem.kt
@@ -0,0 +1,3 @@
+package com.example.nestrecycle
+
+data class ParentItem(val title: String, val logo : Int, val mList: List<ChildItem>)
